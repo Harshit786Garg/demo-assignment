e563dd0c8efd02981c5a90a8798ebe25bfdbc8f7
Harshit786Garg <98043632+Harshit786Garg@users.noreply.github.com>
2022-01-19 22:08:58 +0530

    Add files via upload

---
+++ demo-assignment.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main{
+   public static void main{
+int a=10;
+int b=20;
+System.out.println(a + " " + b);
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a + " " + b);
+
+}
+
+}
